30d4f9107216f0e7dd0da5b842862344f2d40d40
SergeyHazanov <38078324+SergeyHazanov@users.noreply.github.com>
2021-06-19 15:28:54 +0300

    TLS simulation improved.

--- simulation.py
+++ simulation.py
@@ -20,6 +20,7 @@ class TLSSimulation:
         self.final_state = None
 
         self.simulation = None
+        self.bloch_vectors = []
 
     def get_initial_state(self, state_string):
         if state_string == 'ground':
@@ -45,25 +46,33 @@ class TLSSimulation:
         step_func = lambda t, args: 0 if t < t_initial or t > t_initial + duration else 1
         self.H.append([direction[0] * s_x + direction[1] * s_y + direction[2] * s_z, step_func])
 
-    def simulate(self):
+    def simulate(self, store_trajectory=False):
         """
         Simulate the time evolution of the qubit.
         """
-        # Find a way to both store states and get evaluation operators (e_ops).
+        s_x = qt.sigmax()
+        s_y = qt.sigmay()
+        s_z = qt.sigmaz()
+
         self.simulation = qt.sesolve(H=self.H, psi0=self.initial_state, tlist=self.times)
         self.final_state = self.simulation.states[-1]
-        self.simulation = qt.sesolve(H=self.H, psi0=self.initial_state, tlist=self.times,
-                                     e_ops=[qt.sigmax(), qt.sigmay(), qt.sigmaz()])
+
+        if store_trajectory:
+            for state in self.simulation.states:
+                self.bloch_vectors.append([qt.expect(s_x, state), qt.expect(s_y, state), qt.expect(s_z, state)])
 
     def draw_trajectory(self):
         """
         Present 3D bloch sphere with the trajectory of the qubit state as function of time (indicates by the color of
         points.
         """
-        if self.simulation is None:
-            raise Exception(r'No simulation was found.')
+        if not self.bloch_vectors:
+            raise Exception(r'No Bloch vectors were found.')
 
-        xs, ys, zs = self.simulation.expect[0], self.simulation.expect[1], self.simulation.expect[2]
+        xs = np.array([point[0] for point in self.bloch_vectors])
+        ys = np.array([point[1] for point in self.bloch_vectors])
+        zs = np.array([point[2] for point in self.bloch_vectors])
+
         colors = np.linspace(0, 1, len(xs))
         phi = np.linspace(0, np.pi, 20)
         theta = np.linspace(0, 2 * np.pi, 20)
@@ -77,12 +86,18 @@ class TLSSimulation:
         # Set the aspect ratio to 1 so our sphere looks spherical
         fig = plt.figure(figsize=plt.figaspect(1.))
         ax = fig.add_subplot(111, projection='3d')
+        ax.set_box_aspect([1.1, 1.1, 1])
+
+        # plot Bloch sphere and points
         ax.plot_surface(x, y, z, alpha=0.2, cmap='bone', edgecolor='black', linewidth=0.25)
         ax.scatter(xs, ys, zs, s=50, c=colors, cmap='Reds', edgecolors='black')
-        ax.set_box_aspect([1.1, 1.1, 1])
+
+        # add lines
         ax.plot([-1, 1], [0, 0], [0, 0], color='k')
         ax.plot([0, 0], [-1, 1], [0, 0], color='k')
         ax.plot([0, 0], [0, 0], [-1, 1], color='k')
+
+        # add text
         ax.text(0, 0, 1.2, r'$\left|+,z\right\rangle$')
         ax.text(0, 0, -1.2, r'$\left|-,z\right\rangle$')
         ax.text(1.1, 0, 0, r'$\left|+,x\right\rangle$')
@@ -101,7 +116,8 @@ if __name__ == '__main__':
                   'initial_state': 'ground'}
 
     sim = TLSSimulation(**sim_params)
-    sim.add_drive(t_initial=0, duration=2, direction=[1, 0, 0])
-    sim.simulate()
+    sim.add_drive(t_initial=0, duration=1, direction=[1, 0, 0])
+    sim.add_drive(t_initial=1, duration=2, direction=[0, 1, 0])
+    sim.simulate(store_trajectory=True)
     sim.draw_trajectory()
 
